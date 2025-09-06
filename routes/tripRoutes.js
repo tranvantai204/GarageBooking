32a55c2294f7ecd36885d4b6649cade3293e9f85
tranvantai204 <2251120382@ut.edu.vn>
2025-09-06 14:31:31 +0700

    fix ag

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -8,14 +8,15 @@ const { protect, isAdmin } = require('../middleware/authMiddleware');
 // Định nghĩa các routes
 router.route('/')
   // Gắn cả hai middleware vào. Yêu cầu sẽ đi qua 'protect' trước, rồi mới đến 'isAdmin'.
-  .post(protect, isAdmin, createTrip) // <-- SỬA LẠI DÒNG NÀY
+  .post(protect, isAdmin, createTrip) 
   .get(findTrips);
+
+// Place specific routes BEFORE param routes to avoid conflicts
+router.get('/driver/:id/upcoming', getTripsByDriver);
 
 router.route('/:id')
   .get(getTripById)
   .put(protect, isAdmin, updateTrip)
   .delete(protect, isAdmin, deleteTrip);
-
-router.get('/driver/:id/upcoming', getTripsByDriver);
 
 module.exports = router;
